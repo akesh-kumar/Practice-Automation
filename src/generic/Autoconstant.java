5f286f5cecd7a81df39ed946eb1ca497f16ad80a
rakesh <rakeshcutm198@gmail.com>
2023-11-13 16:00:45 +0530

    first commit

---
+++ src/generic/Autoconstant.java
@@ -0,0 +1,11 @@
+package generic;
+
+public interface Autoconstant 
+{
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./drivers/chromedriver.exe";
+	String url="https://timekper.com/";
+	String sheet_path="./testdata/rocky.xlsx";
+	String sheet_name="Sheet1";	
+
+}
